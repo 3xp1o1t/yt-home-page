1d0b529a444d8e61ab420acdb761ddfb5bcd7c8e
3xp1o1t <jes.monti.503@gmail.com>
2023-10-11 21:50:34 -0600

    first commit basic setup

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="max-h-screen flex flex-col bg-sky-300">
+      <div className="bg-rose-300">'1'</div>
+      <div>'2'</div>
+    </div>
+  );
+}
+
+export default App;
