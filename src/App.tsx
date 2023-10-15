2157447bbf2048bb8a0da9c4a915b359ed84aaf2
3xp1o1t <jes.monti.503@gmail.com>
2023-10-14 21:55:10 -0600

    Added: Grid video cards
    Added grid for youtube videos

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import CategoryPills from './components/category-pills';
-import { categories } from './data/home';
+import VideoGridItem from './components/video-grid-item';
+import { categories, videos } from './data/home';
 import PageHeader from './layout/page-header';
 
 function App() {
@@ -9,15 +10,20 @@ function App() {
   return (
     <div className="max-h-screen flex flex-col">
       <PageHeader />
-      <div className="grid grid-cols-[auto,1fr] flex-grow overflow-auto bg-sky-300">
+      <div className="grid grid-cols-[auto,1fr] flex-grow overflow-auto">
         <div>Sidebar</div>
         <div className="overflow-x-hidden px-8 pb-4">
-          <div className="sticky top-0 bg-sky-200 z-10 pb-4">
+          <div className="sticky top-0 z-10 pb-4">
             <CategoryPills
               categories={categories}
               selectedCategory={selectedCategory}
               onSelect={setSelectedCategory}
             />
+          </div>
+          <div className="grid gap-4 grid-cols-[repeat(auto-fill,minmax(300px,1fr))] bg-sky-300">
+            {videos.map((video) => (
+              <VideoGridItem key={video.id} {...video} />
+            ))}
           </div>
         </div>
       </div>
